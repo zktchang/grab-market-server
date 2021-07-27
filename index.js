c034ed5a23600bd0973e8291e87c0dae24de2fce
zktchang <zktchang@gmail.com>
2021-07-27 23:01:49 +0900

    grab-market-server start

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var hostname = "127.0.0.1";
+var port = 8080;
+
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "농구공",
+          price: 5000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+    }
+  }
+  res.end("Good Bye");
+});
+
+server.listen(port, hostname);
+
+console.log("grab market server on!");
